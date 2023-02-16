33b3da45a73779693fed5d675bf913651679115f
Camilo Barreiro <camilobarreiro96@gmail.com>
2023-02-16 08:57:30 -0500

    clean and correct url

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/tutorial-canciones'));
+app.use(express.static(__dirname + '/dist/proyect-frontend'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/tutorial-canciones/index.html'));
+res.sendFile(path.join(__dirname+'/dist/proyect-frontend/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
