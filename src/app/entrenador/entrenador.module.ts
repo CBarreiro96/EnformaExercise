1a9baaf1fabaf0f9eec8e14a9b2fb98860a2f0fb
Benito Zarate Palomec <b.zarate@uniandes.edu.co>
2023-02-19 23:01:26 -0600

    se incluye paginaciÃon

--- src/app/entrenador/entrenador.module.ts
+++ src/app/entrenador/entrenador.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { EntrenadorListaComponent } from './entrenador-lista/entrenador-lista.component';
 import { EncabezadoAppModule } from '../encabezado-app/encabezado-app.module';
 
 @NgModule({
-  imports: [CommonModule, EncabezadoAppModule],
+  imports: [CommonModule, NgbPaginationModule, EncabezadoAppModule],
   declarations: [EntrenadorListaComponent],
 })
 export class EntrenadorModule {}
