45e275941e5f3731c186df1d74e1a588c579590e
Benito Zarate Palomec <b.zarate@uniandes.edu.co>
2023-02-19 23:41:53 -0600

    pruebas unitarias para cobertura de codigo

--- src/app/entrenador/entrenador-lista/entrenador-lista.component.spec.ts
+++ src/app/entrenador/entrenador-lista/entrenador-lista.component.spec.ts
@@ -3,17 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
+import { faker } from '@faker-js/faker';
 import { EntrenadorListaComponent } from './entrenador-lista.component';
 import { EncabezadoComponent } from '../../encabezado-app/encabezado/encabezado.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { Entrenador } from '../entrenador';
+import { EntrenadorService } from '../entrenador.service';
+import { of } from 'rxjs';
 
 describe('EntrenadorListaComponent', () => {
   let component: EntrenadorListaComponent;
   let fixture: ComponentFixture<EntrenadorListaComponent>;
+  let entrenadorService: EntrenadorService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientModule, NgbPaginationModule],
       declarations: [EntrenadorListaComponent, EncabezadoComponent],
     }).compileComponents();
   }));
@@ -29,4 +35,22 @@ describe('EntrenadorListaComponent', () => {
   });
 
   it('should show 10 entrenadores', () => {});
+
+  it('should be load entrenadores', () => {
+    const entrenadores: Entrenador[] = [
+      new Entrenador(
+        faker.datatype.number(),
+        faker.name.firstName(),
+        faker.name.lastName(),
+        faker.address.direction(),
+        faker.phone.number(),
+        [],
+        []
+      ),
+    ];
+    entrenadorService = TestBed.get<EntrenadorService>(EntrenadorService);
+    spyOn(entrenadorService, 'darEntrenadores').and.returnValue(of(entrenadores));
+    component.ngOnInit();
+    expect(component.entrenadores).toEqual(entrenadores);
+  });
 });
