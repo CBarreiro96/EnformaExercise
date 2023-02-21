316169b5e2b0e82417d868d40b651cf31fb261e8
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2023-02-20 22:46:54 -0500

    Comment in the code

--- src/app/usuario/usuario-registro/usuario-registro.component.ts
+++ src/app/usuario/usuario-registro/usuario-registro.component.ts
@@ -11,19 +11,23 @@ import { UsuarioService } from '../usuario.service';
 })
 export class UsuarioRegistroComponent implements OnInit {
 
+  // Declaración de la variable usuarioForm de tipo FormGroup, que representa el formulario de registro
   usuarioForm: FormGroup;
 
   constructor(
-    private usuarioService: UsuarioService,
-    private formBuilder: FormBuilder,
-    private router: Router,
-    private toastrService: ToastrService
+    // Inyección de servicios necesarios para el registro de usuario
+    private usuarioService: UsuarioService, // servicio para interactuar con el backend
+    private formBuilder: FormBuilder, // servicio para construir el formulario
+    private router: Router, // servicio para redirigir a otra página después del registro
+    private toastrService: ToastrService // servicio para mostrar mensajes al usuario
   )
   {
+    // Inicialización del formulario como un FormGroup vacío
     this.usuarioForm = new FormGroup('')
   }
 
   ngOnInit() {
+    // Construcción del formulario mediante el FormBuilder, definiendo los campos del formulario y sus validadores
     this.usuarioForm = this.formBuilder.group({
       usuario: ["", [Validators.required, Validators.maxLength(50)]],
       password: ["", [Validators.required, Validators.maxLength(50), Validators.minLength(4)]],
@@ -33,20 +37,20 @@ export class UsuarioRegistroComponent implements OnInit {
     });
   }
 
+  // Función que se llama cuando se hace clic en el botón de registro
   registrarUsuario() {
+    // Llamada al método de registro del servicio UsuarioService, pasando los valores actuales del formulario
     this.usuarioService.registro(
       this.usuarioForm.get('usuario')?.value, 
       this.usuarioForm.get('password')?.value,
       this.usuarioForm.get('nombre')?.value,
       this.usuarioForm.get('apellido')?.value
       )
-      .subscribe(res => {
+      .subscribe(res => { // Se redirige al usuario a la página principal si el registro fue exitoso
         this.router.navigate([`/`])
       },
-        error => {
+        error => { // Se muestra un mensaje de error si el registro falló
           this.toastrService.error("Error en el registro. Verifique que el usuario no se encuentre ya registrado", "Error", {closeButton: true});
-        })
-  }
-
-
+      })
+    }
 }
