ecea3c8e50b977262bed6e267ce3927aa26e4084
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2023-02-21 10:57:45 -0500

    Add test rutina

--- src/app/rutina/rutina-crear/rutina-crear.component.spec.ts
+++ src/app/rutina/rutina-crear/rutina-crear.component.spec.ts
@@ -9,6 +9,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import { RouterTestingModule } from '@angular/router/testing';
 import { EncabezadoComponent } from 'src/app/encabezado-app/encabezado/encabezado.component';
+import { faker } from '@faker-js/faker';
 
 
 describe('RutinaCrearComponent', () => {
@@ -38,7 +39,28 @@ describe('RutinaCrearComponent', () => {
     fixture.detectChanges();
   });
 
+  // La primera prueba verifica que el componente haya sido creado con éxito.
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  
+  // La segunda prueba verifica que los controles del formulario sean inválidos cuando están vacíos.
+  
+  it('form and controls invalid when empty', () => {
+    expect(component.rutinaForm.valid).toBeFalsy();
+    expect(component.nombre?.valid).toBeFalsy();
+    expect(component.descripcion?.valid).toBeFalsy();
+  });
+  
+  // La tercera prueba verifica que el formulario sea válido cuando se ha completado con datos.
+  
+  it('form valid when is full', () => {
+    // Se completan los campos del formulario con datos aleatorios utilizando la librería Faker.
+    component.nombre?.setValue(faker.datatype.string(50));
+    component.descripcion?.setValue(faker.datatype.string(50));
+  
+    // Se verifica que el formulario sea válido.
+    expect(component.rutinaForm.valid).toBeTruthy()
   });
 });
--- src/app/usuario/usuario-registro/usuario-registro.component.spec.ts
+++ src/app/usuario/usuario-registro/usuario-registro.component.spec.ts
@@ -29,10 +29,12 @@ describe('UsuarioRegistroComponent', () => {
     debug = fixture.debugElement;
   });
 
+  // La primera prueba verifica que el componente haya sido creado con éxito.
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
+  
+  // La segunda prueba verifica que los controles del formulario sean inválidos cuando están vacíos.
   it('form and controls invalid when empty', () => {
     expect(component.usuarioForm.valid).toBeFalsy();
     expect(component.usuario?.valid).toBeFalsy();
@@ -40,14 +42,17 @@ describe('UsuarioRegistroComponent', () => {
     expect(component.nombre?.valid).toBeFalsy();
     expect(component.apellido?.valid).toBeFalsy();
   });
-
+  
+  // La tercera prueba verifica que el formulario sea válido cuando se ha completado con datos.
   it('form valid when is full', () => {
+    // Se completan los campos del formulario con datos aleatorios utilizando la librería Faker.
     component.usuario?.setValue(faker.datatype.string(50));
     component.password?.setValue(faker.datatype.string(50));
     component.confirmpassword?.setValue(faker.datatype.string(50))
     component.nombre?.setValue(faker.datatype.string(60));
     component.apellido?.setValue(faker.datatype.string(60));
-
+    
+    // Se verifica que el formulario sea válido.
     expect(component.usuarioForm.valid).toBeTruthy()
   });
 });
