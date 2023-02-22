61be27ca23fde648f294263002396af8119c3277
Benito Zarate Palomec <b.zarate@uniandes.edu.co>
2023-02-21 22:06:15 -0600

    se agrega unit test

--- src/app/entrenador/entrenador-lista/entrenador-lista.component.spec.ts
+++ src/app/entrenador/entrenador-lista/entrenador-lista.component.spec.ts
@@ -16,6 +16,7 @@ describe('EntrenadorListaComponent', () => {
   let component: EntrenadorListaComponent;
   let fixture: ComponentFixture<EntrenadorListaComponent>;
   let entrenadorService: EntrenadorService;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -28,13 +29,37 @@ describe('EntrenadorListaComponent', () => {
     fixture = TestBed.createComponent(EntrenadorListaComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should show 10 entrenadores', () => {});
+  it('should show 15 entrenadores per page', () => {
+    let entrenadores: Entrenador[] = [];
+    for (let i = 0; i < 30; i++) {
+      entrenadores.push(
+        new Entrenador(
+          faker.datatype.number(),
+          faker.name.firstName(),
+          faker.name.lastName(),
+          faker.address.direction(),
+          faker.phone.number(),
+          [],
+          []
+        )
+      );
+    }
+    entrenadorService = TestBed.get<EntrenadorService>(EntrenadorService);
+    spyOn(entrenadorService, 'darEntrenadores').and.returnValue(
+      of(entrenadores)
+    );
+    component.ngOnInit();
+    fixture.detectChanges();
+    let entrenadoresTabla = debug.queryAll(By.css('table tbody tr'));
+    expect(entrenadoresTabla.length).toEqual(15);
+  });
 
   it('should be load entrenadores', () => {
     const entrenadores: Entrenador[] = [
@@ -49,7 +74,9 @@ describe('EntrenadorListaComponent', () => {
       ),
     ];
     entrenadorService = TestBed.get<EntrenadorService>(EntrenadorService);
-    spyOn(entrenadorService, 'darEntrenadores').and.returnValue(of(entrenadores));
+    spyOn(entrenadorService, 'darEntrenadores').and.returnValue(
+      of(entrenadores)
+    );
     component.ngOnInit();
     expect(component.entrenadores).toEqual(entrenadores);
   });
