c7d163006e89ff64c5c07c172fca74039d22872d
Isai Oliva <i.oliva@uniandes.edu.co>
2023-03-02 00:09:47 -0400

    actualización de funcionalidad

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://enforma2023-backend.herokuapp.com'
+    production: false,
+    apiUrl: 'http://localhost:5000'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://enforma2023-backend.herokuapp.com'
+    production: true,
+    apiUrl: 'http://localhost:5000'
   };
