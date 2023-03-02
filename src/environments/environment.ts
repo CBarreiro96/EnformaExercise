0013d19bb63146c6190e91129379901cd073dcc1
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2023-03-01 22:54:49 -0500

    add hotfix  add rutinas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://enforma2023-backend.herokuapp.com'
+    apiUrl: 'http://127.0.0.1:5000'
+    //apiUrl: 'https://enforma2023-backend.herokuapp.com'
   };
