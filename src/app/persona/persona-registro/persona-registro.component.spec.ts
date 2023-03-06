e52cb806ed1c9965029618d0eeb998f1569086e8
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2023-03-05 23:05:16 -0500

    add test

--- src/app/persona/persona-registro/persona-registro.component.spec.ts
+++ src/app/persona/persona-registro/persona-registro.component.spec.ts
@@ -10,6 +10,7 @@ import {RouterTestingModule} from "@angular/router/testing";
 import {ReactiveFormsModule} from "@angular/forms";
 import { HttpClientModule } from '@angular/common/http';
 import {catchError, of, throwError} from "rxjs";
+import {faker} from "@faker-js/faker";
 
 describe('PersonaRegistroComponent', () => {
   let component: PersonaRegistroComponent;
@@ -36,6 +37,24 @@ describe('PersonaRegistroComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // La segunda prueba verifica que los controles del formulario sean inválidos cuando están vacíos.
+  it('form and controls invalid when empty', () => {
+    expect(component.personaForm.valid).toBeFalsy();
+    expect(component.usuario?.valid).toBeFalsy();
+    expect(component.password?.valid).toBeFalsy();
+  });
+
+  // La tercera prueba verifica que el formulario sea válido cuando se ha completado con datos.
+  it('form valid when is full', () => {
+    // Se completan los campos del formulario con datos aleatorios utilizando la librería Faker.
+    component.usuario?.setValue(faker.datatype.string(50));
+    component.password?.setValue(faker.datatype.string(50));
+    component.confirmpassword?.setValue(faker.datatype.string(50))
+
+    // Se verifica que el formulario sea válido.
+    expect(component.personaForm.valid).toBeTruthy()
   });
 
 });
