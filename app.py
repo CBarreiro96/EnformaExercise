b89a52a00e6596cd900e29f97571ac2a4b496fdb
david-andres-leon <da.vargasl1@uniandes.edu.co>
2023-03-07 21:37:09 -0500

    enpoint creado, con sus pruebas correspondiente para crear credenciales

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from vistas import \
     VistaPersona, VistaPersonas, \
     VistaEjercicio, VistaEjercicios, \
     VistaEntrenamiento, VistaEntrenamientos, VistaRutina, \
-    VistaReporte, VistaEntrenadores, VistaRutinas
+    VistaReporte, VistaEntrenadores, VistaRutinas, VistaCliente
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dbapp.sqlite'
@@ -38,6 +38,7 @@ api.add_resource(VistaReporte, '/persona/<int:id_persona>/reporte')
 api.add_resource(VistaEntrenadores, '/entrenadores')
 api.add_resource(VistaRutina, '/rutina/<int:id_usuario>')
 api.add_resource(VistaRutinas, '/rutinas/<int:id_usuario>')
+api.add_resource(VistaCliente, '/signin/cliente')
 
 jwt = JWTManager(app)
 
