05ec8f9ddfe48362616588d8f4733a6f6b79be62
david-andres-leon <da.vargasl1@uniandes.edu.co>
2023-03-09 23:15:20 -0500

    MERGE

--- vistas/vistas.py
+++ vistas/vistas.py
@@ -74,12 +74,13 @@ class VistaLogIn(Resource):
 class VistaPersonas(Resource):
     @jwt_required()
     def get(self, id_usuario):
-        usuario = Usuario.query.get_or_404(id_usuario)
-        return [persona_schema.dump(persona) for persona in usuario.personas]
+        entrenador = Entrenador.query.filter(Entrenador.usuario == id_usuario).first()
+        return [persona_schema.dump(persona) for persona in entrenador.personas]
 
     @jwt_required()
     def post(self, id_usuario):
         usuario = Usuario.query.get_or_404(id_usuario)
+        entrenador = Entrenador.query.filter(Entrenador.usuario == id_usuario).first()
         nueva_persona = Persona( \
             nombre=request.json["nombre"], \
             apellido=request.json["apellido"], \
@@ -94,10 +95,8 @@ class VistaPersonas(Resource):
             entrenando=bool(request.json["entrenando"]), \
             razon=request.json["razon"], \
             terminado=datetime.strptime(request.json["terminado"], '%Y-%m-%d'), \
-            entrenador=usuario \
+            entrenador=entrenador.id \
             )
-        usuario.personas.append(nueva_persona)
-        db.session.add(usuario)
         db.session.add(nueva_persona)
         db.session.commit()
         return persona_schema.dump(nueva_persona)
@@ -290,6 +289,15 @@ class VistaRutina(Resource):
             return False
         else:
             return True
+class VistaEjercicioRutina (Resource):
+    def put(self):
+        id_rutina = request.json["id_rutina"]
+        id_ejercicio = request.json["id_ejercicio"]
+        rutina = Rutina.query.get_or_404(id_rutina)
+        ejercicio = Ejercicio.query.get_or_404 (id_ejercicio)
+        rutina.ejercicios.append(ejercicio)
+        db.session.commit()
+        return rutina_schema.dump(rutina), 201
 
 
 class VistaRutinas(Resource):
